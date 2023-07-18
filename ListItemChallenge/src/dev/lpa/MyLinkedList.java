038ef4cf9bdac7cdc0eeb7d46f770a3d27d89054
tobikasaba <oluwatobikasaba12@gmail.com>
2023-07-18 23:38:40 +0100

    included implementation for NodeList and MyLinkedList. still need to work on methods

--- ListItemChallenge/src/dev/lpa/MyLinkedList.java
+++ ListItemChallenge/src/dev/lpa/MyLinkedList.java
@@ -1,5 +1,41 @@
 package dev.lpa;
 
 
-public class MyLinkedList {
+public class MyLinkedList implements NodeList {
+
+
+    private ListItem root;
+
+    public MyLinkedList(ListItem root) {
+        this.root = root;
+    }
+
+
+    @Override
+    public ListItem getRoot() {
+        return root;
+    }
+
+    @Override
+    public boolean addItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public boolean removeItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public void traverse(ListItem root) {
+        if (root == null) {
+            System.out.println("The list is empty");
+        }else {
+            while (root != null){
+                System.out.println(root.getValue());
+                root = root.next();
+            }
+        }
+
+    }
 }
--- ListItemChallenge/src/dev/lpa/NodeList.java
+++ ListItemChallenge/src/dev/lpa/NodeList.java
@@ -1,4 +1,9 @@
 package dev.lpa;
 
 public interface NodeList {
+
+    abstract ListItem getRoot();
+    abstract boolean addItem(ListItem item);
+    abstract boolean removeItem(ListItem item);
+    abstract void traverse(ListItem root);
 }
