e2951b0c64fcd63261d5edfab974c01810aeec0e
tobikasaba <oluwatobikasaba12@gmail.com>
2023-07-19 23:06:02 +0100

    working on completing addItem method in MyLinkedList class

--- ListItemChallenge/src/dev/lpa/MyLinkedList.java
+++ ListItemChallenge/src/dev/lpa/MyLinkedList.java
@@ -17,8 +17,22 @@ public class MyLinkedList implements NodeList {
     }
 
     @Override
-    public boolean addItem(ListItem item) {
-        return false;
+    public boolean addItem(ListItem newItem) {
+        if (root == null) {
+            root = newItem;
+            return true;
+        }
+
+        ListItem currentItem = root;
+        while (currentItem != null) {
+            int comparison = (currentItem.compareTo(newItem));
+            if (comparison < 0) {
+
+            }
+//            if (currentItem.value > newItem.value) {
+
+            }
+        }
     }
 
     @Override
@@ -30,8 +44,8 @@ public class MyLinkedList implements NodeList {
     public void traverse(ListItem root) {
         if (root == null) {
             System.out.println("The list is empty");
-        }else {
-            while (root != null){
+        } else {
+            while (root != null) {
                 System.out.println(root.getValue());
                 root = root.next();
             }
