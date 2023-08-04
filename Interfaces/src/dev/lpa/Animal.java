ecb86e6860effcfcb1c745fa489b79068052b9d7
Tobi Kasaba <oluwatobikasaba12@gmail.com>
2023-08-04 16:10:01 +0100

    completed interface video 1 and 2 and 3. - using interface type as the reference type

--- Interfaces/src/dev/lpa/Animal.java
+++ Interfaces/src/dev/lpa/Animal.java
@@ -1,13 +1,12 @@
 package dev.lpa;
-
-
+interface OrbitEarth extends FlightEnabled {
+    void achieveOrbit();
+}
 interface FlightEnabled {
-
-    /*any fields declared on an interface are not instance fields, they are explicitly constant, public and static*/
+    /*any fields declared on an interface are not instance fields, they are explicitly constant, public, final and static*/
     double MILES_TO_KM = 1.60934;
 
     double KM_TO_MILES = 0.621371;
-
 
     /*
      * abstract modifier is implicitly declared for all interfaces, has this doesn't have to be declared
@@ -15,16 +14,58 @@ interface FlightEnabled {
      */
 
     void takeOff();
-
     void land();
-
     void fly();
 }
-
 interface Trackable {
-
     void track();
+}
+enum FlightStages implements Trackable {GROUNDED, LAUNCH, CRUISE, DATA_COLLECTION;
 
+    @Override
+    public void track() {
+        if(this!= GROUNDED){
+            System.out.println("Monitoring " + this);
+        }
+    }
+}
+/**
+ * Normally, records don't have a body but because this record implements the FlightEnabled interface,
+ * it has to override the abstract methods in the interface*/
+record DragonFly(String name, String type) implements FlightEnabled {
+
+    @Override
+    public void takeOff() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
+class Satellite implements OrbitEarth {
+
+    public void achieveOrbit() {
+        System.out.println("Orbit achieved");
+    }
+
+    @Override
+    public void takeOff() {
+    }
+
+    @Override
+    public void land() {
+    }
+
+    @Override
+    public void fly() {
+    }
 }
 
 public abstract class Animal {
--- Interfaces/src/dev/lpa/Main.java
+++ Interfaces/src/dev/lpa/Main.java
@@ -1,6 +1,9 @@
 package dev.lpa;
 
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Bird bird = new Bird();
@@ -24,16 +27,34 @@ public class Main {
 */
 
         inFlight(flier);
-        inFlight(new Jet ());
+        inFlight(new Jet());
         Trackable truck = new Truck();
         truck.track();
 
         double kmsTraveled = 100;
         double milesTraveled = kmsTraveled * FlightEnabled.KM_TO_MILES;
         System.out.printf("The truck traveled %.2f kmm or %.2f miles %n", kmsTraveled, milesTraveled);
+
+
+        /*when working with interfaces, it's recommended to use the interface type for the declared
+        * variable
+        * So while ArrayList is does compile, it's not best practice because ArrayList uses the List interface*/
+        LinkedList<FlightEnabled> fliers = new LinkedList<>();
+        fliers.add(bird);
+
+        List<FlightEnabled> betterFliers = new LinkedList<>();
+        betterFliers.add(bird);
+
+        triggerFliers(fliers);
+        flyFliers(fliers);
+        landFliers(fliers);
+
+        triggerFliers(betterFliers);
+        flyFliers(betterFliers);
+        landFliers(betterFliers);
     }
 
-    private static void inFlight(FlightEnabled flier){
+    private static void inFlight(FlightEnabled flier) {
 
         flier.takeOff();
         flier.fly();
@@ -42,6 +63,26 @@ public class Main {
             tracked.track();
         }
         flier.land();
+    }
 
+    private static void triggerFliers(List<FlightEnabled> fliers) {
+
+        for (var flier : fliers) {
+            flier.takeOff();
+        }
+    }
+
+    private static void flyFliers(List<FlightEnabled> fliers) {
+
+        for (var flier : fliers) {
+            flier.fly();
+        }
+    }
+
+    private static void landFliers(List<FlightEnabled> fliers) {
+
+        for (var flier : fliers) {
+            flier.land();
+        }
     }
 }
