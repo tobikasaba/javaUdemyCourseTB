bb72f628d3b8d0ba25ee72e7a6786878c098fc25
Tobi Kasaba <oluwatobikasaba12@gmail.com>
2023-10-01 20:50:37 +0100

    Generics methods, wildcards and type erasure.

--- GenericsExtra/src/dev/lpa/Main.java
+++ GenericsExtra/src/dev/lpa/Main.java
@@ -18,17 +18,17 @@ public class Main {
 //        printList(students);
         printMoreList(students);
 
-//        List<LPAStudent> lpaStudents = new ArrayList<>();
-//        for (int i = 0; i < studentCount; i++) {
-//            lpaStudents.add(new LPAStudent());
-//        }
-//        students.add(new LPAStudent());
-//
-////        printList(lpaStudents);
-//        printMoreList(lpaStudents);
-//
-//        testList(new ArrayList<String>(List.of("Able", "Barry","Charlie")));
-//        testList(new ArrayList<Integer>(List.of(1,2,3)));
+        List<LPAStudent> lpaStudents = new ArrayList<>();
+        for (int i = 0; i < studentCount; i++) {
+            lpaStudents.add(new LPAStudent());
+        }
+        students.add(new LPAStudent());
+
+//        printList(lpaStudents);
+        printMoreList(lpaStudents);
+
+        testList(new ArrayList<String>(List.of("Able", "Barry","Charlie")));
+        testList(new ArrayList<Integer>(List.of(1,2,3)));
     }
 
     /**
