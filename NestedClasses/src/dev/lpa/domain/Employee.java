a2748decb6daae328460ff4f128e6a5d6d21ebf8
Tobi Kasaba <oluwatobikasaba12@gmail.com>
2023-10-05 19:12:59 +0100

    implementing and instantiating nested inner class

--- NestedClasses/src/dev/lpa/domain/Employee.java
+++ NestedClasses/src/dev/lpa/domain/Employee.java
@@ -4,8 +4,31 @@ import java.util.Comparator;
 
 public class Employee {
 
+    private int employeeId;
+    private String name;
+    private int yearStarted;
+
+    public Employee() {
+    }
+
+    public Employee(int employeeId, String name, int yearStarted) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.yearStarted = yearStarted;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "%d %-8s %d".formatted(employeeId, name, yearStarted);
+    }
+
     /**
      * Employee Comparator is a Static Nested Class
+     *
      * @param <T>
      */
     public static class EmployeeComparator<T extends Employee>
@@ -23,37 +46,12 @@ public class Employee {
 
         @Override
         public int compare(Employee o1, Employee o2) {
-            if (sortType.equalsIgnoreCase("yearStarted")){
-                        return o1.yearStarted-o2.yearStarted;
+            if (sortType.equalsIgnoreCase("yearStarted")) {
+                return o1.yearStarted - o2.yearStarted;
             }
             return o1.getName().compareTo(o2.getName());
 
         }
-    }
-
-    private int employeeId;
-
-    private String name;
-
-    private int yearStarted;
-
-    public Employee() {
-    }
-
-
-    public Employee(int employeeId, String name, int yearStarted) {
-        this.employeeId = employeeId;
-        this.name = name;
-        this.yearStarted = yearStarted;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public String toString() {
-        return "%d %-8s %d".formatted(employeeId, name, yearStarted);
     }
 
 
