cf5ebb0824cd06c9923145f4203db36617c4b954
Tobi Kasaba <oluwatobikasaba12@gmail.com>
2023-10-11 16:36:15 +0100

    added project files that were'nt included before

---
+++ PlaylistInnerClassChallenge/src/dev/lpa/Song.java
@@ -0,0 +1,24 @@
+package dev.lpa;
+
+public class Song {
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    /**
+     * The point of overriding the toString methods means you want the object to print
+     * a particular thing when you "sout" print out the object.
+     * by overriding, what you put in is what is printed instead of the objects memory
+     */
+    @Override
+    public String toString() {
+        return "" + title + ": " + duration;
+    }
+}
