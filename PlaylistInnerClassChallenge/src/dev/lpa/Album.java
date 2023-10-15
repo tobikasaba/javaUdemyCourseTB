edc21d3ef6265cdd222b924b97db2fd199c077b0
tobikasaba <oluwatobikasaba12@gmail.com>
2023-10-15 14:38:01 +0100

    Completed Playlist challenge for inner class: -explained why the get() & size() methods could not be used for the songs field in the Album class

--- PlaylistInnerClassChallenge/src/dev/lpa/Album.java
+++ PlaylistInnerClassChallenge/src/dev/lpa/Album.java
@@ -2,79 +2,102 @@ package dev.lpa;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 
 public class Album {
 
     private String name, artist;
-    private ArrayList<Song> songs;
+    private SongList songs;
 
     public Album(String name, String artist) {
         this.name = name;
         this.artist = artist;
-        this.songs = new ArrayList<>();
+        this.songs = new SongList();
     }
 
     public boolean addSong(String title, double duration) {
         Song newSong = new Song(title, duration);
-        if (findSong(title) == null) {
+        if (songs.findSong(newSong.getTitle()) == null) {
             songs.add(newSong);
-            System.out.println("song added" + songs);
             return true;
         }
         return false;
+//        return this.songs.add(new Song(title, duration));
     }
 
-    private Song findSong(String title) {
-        for (Song songTitle : songs) {
-            if (songTitle.getTitle().equals(title)) {
-                return songTitle;
-            }
-        }
-        return null;
-    }
-
-    public boolean addToPlayList(String title, LinkedList<Song> playList) {
-        var song = findSong(title);
-        if (song != null) {
-            playList.add((song));
-            System.out.println("title playlist " + playList);
+    public boolean addToPlayList(int track, LinkedList<Song> playlist) {
+        Song checkSong = songs.findSong(track);
+        if (checkSong != null) {
+            playlist.add(checkSong);
             return true;
         }
-        return false;
-    }
-
-    public boolean addToPlayList(int track, LinkedList<Song> playList) {
+/*
+in this code block, songs.size() and songs.get() throw an error. This is because songs is a field of type SongList which doesn't have these methods in it
         for (int i = 0; i < songs.size(); i++) {
             if (track > 0 && i == track - 1) {
-                playList.add(songs.get(i));
-                System.out.println("track playlist " + playList);
+                playlist.add(songs.get(i));
+                System.out.println("track playlist " + playlist);
                 return true;
             }
         }
+ */
         return false;
     }
 
-    public class SongList {
+    public boolean addToPlayList(String title, LinkedList<Song> playlist) {
+        Song checkSong = songs.findSong(title);
+        if (checkSong != null) {
+            playlist.add(checkSong);
+            return true;
+        }
+/*
+in this code block, songs.size() and songs.get() throw an error. This is because songs is a field of type SongList which doesn't have these methods in it
+        var song = songs.findSong(title);
+        if (song != null) {
+            playlist.add((song));
+            System.out.println("title playlist " + playlist);
+            return true;
+        }
+ */
+        return false;
+    }
 
-        private List<Song> songs = new ArrayList<>();
+    public static class SongList {
 
-        public SongList() {
+        private ArrayList<Song> songs;
+
+        private SongList() {
             songs = new ArrayList<>();
         }
 
-        private boolean findSong (String songTitle){
-            for (var song : songs){
-                if(song.getTitle().equals(songTitle)){
+        private boolean add(Song songName) {
+            for (var song : songs) {
+                if (song.getTitle().equals(songName.getTitle())) {
+                    System.out.println("song present " + songName);
                     return false;
-                }else {
-                    songs.add(newSong);
-                    return true;
                 }
             }
-            return false;
+            songs.add(songName);
+            System.out.println("song added " + songName);
+            return true;
+
         }
 
+        private Song findSong(String title) {
+            for (var songTitle : songs) {
+                if (songTitle.getTitle().equals(title)) {
+                    return songTitle;
+                }
+            }
+            return null;
+        }
 
+        private Song findSong(int track) {
+            for (var i = 0; i < songs.size(); i++) {
+                if (track > 0 && i == track - 1) {
+                    return songs.get(i);
+                }
+            }
+            return null;
+        }
     }
 }
