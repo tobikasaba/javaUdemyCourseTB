370242beb9ad1a6fcebaf5c7c00abed41a430be9
Tobi Kasaba <oluwatobikasaba12@gmail.com>
2023-10-24 18:29:10 +0100

    Covered custom functional interface in Java: - created a custom functional interface and how to use it - how to declare typed in lambdas (mixing of explicitly typed parameters not allowed) - implementing and customising lambda for multipurpose usages - showed how return statement can be implemented in lambda expressions (return statements can only be used in a code block)

--- LambdaExpressions/src/dev/lpa/Main.java
+++ LambdaExpressions/src/dev/lpa/Main.java
@@ -27,5 +27,32 @@ public class Main {
             char first = myString.charAt(0);
             System.out.println(prefix + " " + myString + " means " + first);
         });
+
+        int result = calculator((Integer a, Integer b) -> {
+            return a + b;
+        }, 5, 2);
+        var result2 = calculator((var a, var b) -> {
+            double c = a / b;
+            return c;
+        }, 10.0, 2.5);
+        var result3 = calculator((var a, var b) -> a / b, 10.0, 2.5);
+        var result4 = calculator((a, b) -> a.toUpperCase() + " " + b.toUpperCase(), "Ralph", "Kramden");
+
+    }
+
+    /**
+     * This calculator method is used to perform different operations on different types of data.
+     *
+     * @param function
+     * @param value1
+     * @param value2
+     * @param <T>
+     * @return
+     */
+    public static <T> T calculator(Operation<T> function, T value1, T value2) {
+
+        T result = function.operate(value1, value2);
+        System.out.println("Result of operation: " + result);
+        return result;
     }
 }
