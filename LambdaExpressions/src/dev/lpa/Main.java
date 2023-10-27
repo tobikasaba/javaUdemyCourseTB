d7b5d32610f961db07a9be8215ae73b35773e2af
Tobi Kasaba <oluwatobikasaba12@gmail.com>
2023-10-27 17:15:45 +0100

    Covered inbuilt functional interface Consumer and BiConsumer in Java: - Assigning lambda expressions to variables - executing functional interface methods directly on the variables - passing variables in methods containing functional interface - covered remove if method inbuilt method

--- LambdaExpressions/src/dev/lpa/Main.java
+++ LambdaExpressions/src/dev/lpa/Main.java
@@ -46,9 +46,39 @@ public class Main {
                 new double[]{29.1566, -89.2495},
                 new double[]{35.1556, -90.0659});
 
+//        using forEach Method to iterate over the coords List and then using Arrays.toString to print each value of the array out
         coords.forEach(s -> System.out.println(Arrays.toString(s)));
 
+        /*
+        lambda expression can be assigned to a local variable. As you can see below.
+        Declaring a variable with a lambda expression doesn't execute it. To do so, execute the functional method on the variable or pass the variable to a method that does the same thing
+        In this, the lambda expression defines what should happen to the arguments passed into the accept method of the BiConsumer interface.
+         */
         BiConsumer<Double, Double> p1 = (lat, lng) -> System.out.printf("[lat:%.3f lon:%3f]%n", lat, lng);
+
+        var firstPoint = coords.get(0);
+
+//        execute the functional method on the variable
+        p1.accept(firstPoint[0], firstPoint[1]);
+
+//        pass the variable to a method that does the same thing
+        processPoint(firstPoint[0], firstPoint[1], p1);
+
+        System.out.println("_____");
+        coords.forEach(s -> processPoint(s[0], s[1], p1));
+        coords.forEach(s -> processPoint(s[0], s[1],
+                (lat, lng) -> System.out.printf("[lat:%.3f lon:%3f]%n", lat, lng)));
+
+        list.removeIf(s -> s.equalsIgnoreCase("bravo"));
+        list.forEach(s -> System.out.println(s));
+
+        System.out.println("---------");
+        list.addAll(List.of("echo", "easy", "earnest"));
+        list.forEach(s -> System.out.println(s));
+
+        System.out.println("---------");
+        list.removeIf((s -> s.startsWith("ea")));
+        list.forEach(s -> System.out.println(s));
     }
 
 
@@ -59,6 +89,14 @@ public class Main {
         return result;
     }
 
+    /**
+     * This method allows reference calls to the BiConsumer functional interface.
+     *
+     * @param t1       first argument of the method of generic type
+     * @param t2       second argument of the method generic type
+     * @param consumer variable
+     * @param <T>      generic parameter which allows access to the method in the BiConsumer interface
+     */
     public static <T> void processPoint(T t1, T t2, BiConsumer<T, T> consumer) {
         consumer.accept(t1, t2);
     }
