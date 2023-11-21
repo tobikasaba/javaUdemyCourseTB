e85811d7fadd8d0f757430244f436da7580b20c5
Tobi Kasaba <oluwatobikasaba12@gmail.com>
2023-11-21 16:56:31 +0000

    channing lambda expressions

--- ChanningLambdas/src/Main.java
+++ ChanningLambdas/src/Main.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,5 +25,75 @@ public class Main {
  */
         uCaseLastName = uCase.compose(lastName);
         System.out.println(uCaseLastName.apply(name));
+
+        /*
+        when channing lambda methods, it doesnt matter if the type used in the channing matches the expected return type.
+        what matters is that the type returned in a method matches the expected type in a following method
+        e.g if a method returns type string, the next method accepts a type string
+         */
+        Function<String, String[]> f0 = uCase
+                .andThen(s -> s.concat(" Kasaba"))
+                .andThen(s -> s.split(" "));
+        System.out.println(Arrays.toString(f0.apply(name)));
+
+        Function<String, String> f1 = uCase
+                .andThen(s -> s.concat(" Kasaba"))
+                .andThen(s -> s.split(" "))
+                .andThen(s -> s[1].toUpperCase() + ", " + s[0]);
+
+        System.out.println(f1.apply(name));
+        Function<String, Integer> f2 = uCase
+                .andThen(s -> s.concat(" Kasaba"))
+                .andThen(s -> s.split(" "))
+                .andThen(s -> String.join(", ", s))
+                .andThen(String::length);
+        System.out.println((f2.apply(name)));
+
+        System.out.println("----------");
+        String[] names = {"Anna", "Bob", "Carol"};
+        Consumer<String> s0 = s -> System.out.println(s.charAt(0));
+        Consumer<String> s1 = System.out::println;
+        Arrays.asList(names).forEach(s0
+                .andThen(s -> System.out.println(" - "))
+                .andThen(s1));
+
+        Predicate<String> p1 = s -> s.equals("TOBI");
+        Predicate<String> p2 = s -> s.equalsIgnoreCase("TOBI");
+        Predicate<String> p3 = s -> s.startsWith("T");
+        Predicate<String> p4 = s -> s.endsWith("i");
+
+        Predicate<String> combined1 = p1.or(p2);
+        System.out.println("combined1 = " + combined1.test(name));
+
+        Predicate<String> combined2 = p3.and(p4);
+        System.out.println("combined2 = " + combined2.test(name));
+
+//        negate returns the opposite result from the result of the previous functions
+        Predicate<String> combined3 = p3.and(p4).negate();
+        System.out.println("combined3 = " + combined3.test(name));
+
+        record Person(String firstName, String lastName) {
+        }
+        List<Person> list = new ArrayList<>(Arrays.asList(
+                new Person("Peter", "Pan"),
+                new Person("Peter", "PumpkinEater"),
+                new Person("Minnie", "Mouse"),
+                new Person("Mickey", "Mouse")
+        ));
+
+        list.sort(((o1, o2) -> o1.lastName.compareTo(o2.lastName)));
+        list.forEach(System.out::println);
+
+        System.out.println("---------------------");
+        list.sort(Comparator.comparing(Person::lastName));
+        list.forEach(System.out::println);
+
+
+        System.out.println("---------------------");
+        list.sort(Comparator.comparing(Person::lastName)
+                .thenComparing(Person::firstName).reversed());
+        list.forEach(System.out::println);
+
+
     }
 }
