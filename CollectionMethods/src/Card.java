f48fa8fb44a5a4666811c462366cd8766bf8aeb7
tobikasaba <oluwatobikasaba12@gmail.com>
2023-12-11 18:06:06 +0000

    Working with methods in the Collections Class and how to use these methods such as addAll() and copy().

--- CollectionMethods/src/Card.java
+++ CollectionMethods/src/Card.java
@@ -3,13 +3,13 @@ import java.util.List;
 
 public record Card(Suit suit, String face, int rank) {
 
-//    public enum Suit {
-//        CLUB, DIAMOND, HEART, SPADE;
-//
-//        public char getImage() {
-//            return (new char[]{9827, 9830, 9829, 9824})[this.ordinal()];
-//        }
-//    }
+    public enum Suit {
+        CLUB, DIAMOND, HEART, SPADE;
+
+        public char getImage() {
+            return (new char[]{9827, 9830, 9829, 9824})[this.ordinal()];
+        }
+    }
 
     @Override
     public String toString() {
--- CollectionMethods/src/Main.java
+++ CollectionMethods/src/Main.java
@@ -1,18 +1,45 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        char ch = 9827;
-        int ascii = ch;
-        System.out.println("The ASCII value of " + ch);
 
         List<Card> deck = Card.getStandardDeck();
         Card.printDeck(deck);
 
+        Card[] cardArray = new Card[13];
+        Card aceOfHearts = Card.getFaceCard(Card.Suit.HEART, 'A');
+        Arrays.fill(cardArray, aceOfHearts);
+        Card.printDeck(Arrays.asList(cardArray), "Aces of Hearts", 1);
 
-//        System.out.println(test);
+/*
+        placing 52 in an arrayList list does not fill the arrayList with 52 null values like it would an array
+        here the fill method won't work because the cards list has no values in it
+        therefore, it has fewer elements that the items being filled
+ */
+        List<Card> cards = new ArrayList<>(52);
+        Collections.fill(cards, aceOfHearts);
+        System.out.println(cards);
+        System.out.println("cards.size() = " + cards.size());
 
-//        System.out.println(Card.getFaceCard((char) 9830, "Q"));
-//        System.out.println(Card.getNumericCard((char) 9827, 3));
-;    }
+        List<Card> acesOfHearts = Collections.nCopies(13, aceOfHearts);
+        Card.printDeck(acesOfHearts, "Aces of Hearts", 1);
+
+        Card kingOfClubs = Card.getFaceCard(Card.Suit.CLUB, 'K');
+        List<Card> kingsOfClubs = Collections.nCopies(13, kingOfClubs);
+        Card.printDeck(kingsOfClubs, "Kings Of Clubs", 1);
+
+        Collections.addAll(cards, cardArray);
+        Collections.addAll(cards, cardArray);
+        Card.printDeck(cards, "Card Collection with Aces added", 2);
+
+        Collections.copy(cards, kingsOfClubs);
+        Card.printDeck(cards, "Card Collection with Kings copied", 1);
+
+//        this method returns a copy of the list but this is immutable (unmodifiable)
+        cards = List.copyOf(kingsOfClubs);
+        Card.printDeck(cards, "List Copy of Kings", 1);
+    }
 }
