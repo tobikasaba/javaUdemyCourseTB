c8b286d796d533bd7dd7387cdf758d650857d798
tobikasaba <oluwatobikasaba12@gmail.com>
2023-12-28 19:39:16 +0000

    completing collection card challenge

---
+++ CollectionMethods/src/dev/lpa/Suit.java
@@ -0,0 +1,11 @@
+package dev.lpa;
+
+public enum Suit {
+    CLUB, DIAMOND, HEART, SPADE;
+
+    public char getImage() {
+        return (new char[]{9827, 9830, 9829, 9824})[this.ordinal()];
+    }
+}
+
+
