3b8e38624e9b9be6f9dbab610388f35c2da8fb01
tobikasaba <oluwatobikasaba12@gmail.com>
2024-01-08 12:25:57 +0000

    Setting up the Task Class fot the Set Operations Challenge

---
+++ SetOperationsChallenge/src/dev/lpa/Task.java
@@ -0,0 +1,105 @@
+package dev.lpa;
+
+enum Status {IN_QUEUE, ASSIGNED, IN_PROGRESS}
+
+enum Priority {HIGH, LOW, MEDIUM}
+
+public class Task implements Comparable<Task> {
+
+
+    private String assignee;
+    private String project;
+    private String description;
+    private Status status;
+    private Priority priority;
+
+    public Task(String assignee, String project, String description, Status status, Priority priority) {
+        this.assignee = assignee;
+        this.project = project;
+        this.description = description;
+        this.status = status;
+        this.priority = priority;
+    }
+
+    public Task(String assignee, String project, String description, Priority priority) {
+        this(assignee, project, description, assignee == null ? Status.IN_QUEUE : Status.ASSIGNED, priority);
+
+    }
+
+    public Task(String project, String description, Priority priority) {
+        this(null, project, description, priority);
+    }
+
+    public void setAssignee(String assignee) {
+        this.assignee = assignee;
+    }
+
+    public void setProject(String project) {
+        this.project = project;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public String getAssignee() {
+        return assignee;
+    }
+
+    public String getProject() {
+        return project;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Task task = (Task) o;
+
+        if (!getProject().equals(task.getProject())) return false;
+        return getDescription().equals(task.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getProject().hashCode();
+        result = 31 * result + getDescription().hashCode();
+        return result;
+    }
+
+    @Override
+    public int compareTo(Task o) {
+        int result = this.project.compareTo(o.project);
+
+        if (result == 0) {
+            result = this.description.compareTo(o.description);
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "%-20s %-25s %-10s %-10s %s".formatted(project, description, priority, assignee, status);
+    }
+}
